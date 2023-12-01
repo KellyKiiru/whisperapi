99389c706208e094bc65adafe70bd10d9df72390
kellykiiru <infowithkiiru@gmail.com>
2023-12-01 23:00:39 +0300

    changers

---
+++ transcribe.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+audio_file_path = "EarningsCall.wav"
+
+from docx import Document
+
+def transcribe_audio(audio_file_path):
+    with open(audio_file_path, 'rb') as audio_file:
+        transcription = client.audio.transcriptions.create("whisper-1", audio_file)
+    return transcription['text']
