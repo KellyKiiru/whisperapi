86fb65ef4ba2c6aa2a84b745125d113e8a903e51
kellykiiru <infowithkiiru@gmail.com>
2023-12-04 23:24:16 +0300

    changes

--- transcribe.py
+++ transcribe.py
@@ -16,3 +16,4 @@ def transcribe_audio(audio_file_path):
     with open(audio_file_path, 'rb') as audio_file:
         transcription = client.audio.transcriptions.create("whisper-1", audio_file)
     return transcription['text']
+
